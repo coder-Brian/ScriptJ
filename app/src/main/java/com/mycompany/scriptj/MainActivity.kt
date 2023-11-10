bd30442a96af8f22fc9b677109d57ba013e1c2c0
coder-Brian <researchers579@gmail.com>
2023-11-10 16:27:26 +0800

    first commit

---
+++ app/src/main/java/com/mycompany/scriptj/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.mycompany.scriptj
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        Thread.sleep(1000)
+        installSplashScreen()
+
+        setContentView(R.layout.activity_main)
+    }
+}
